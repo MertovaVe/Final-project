85a7ca15cedaa72a05ceb83225a2084414568c60
naturechen <naturechen0626@gmail.com>
2022-02-04 19:01:34 +0100

    Add market_index function
    Add market_index function

--- Control_Code.py
+++ Control_Code.py
@@ -20,7 +20,7 @@ class Control:
         url = 'https://yfapi.net/v8/finance/spark'
 
         query_string = {
-            'symbols': 'AAPL,MSFT',
+            'symbols': 'AAPL,MSFT,TSLA',
             'interval': '1d',
             'range': '1y'
         }
@@ -30,15 +30,26 @@ class Control:
     
         stockData = modleObj.get_Finance_Data()
 
-        return self.parse_Data(stockData)
+        stockDF = self.parse_Data(stockData)
 
 
-    def get_ROE_Data(self):
+        for x in range(len(stockDF.loc['timestamp'])):
+            for y in range(len(stockDF.loc['timestamp'][x])):
+                stockDF.loc['timestamp'][x][y] = datetime.fromtimestamp(stockDF.loc['timestamp'][x][y])
+            
 
-        url = 'https://yfapi.net/v11/finance/quoteSummary/AAPL'
+        #print(datetime.fromtimestamp(stockDF.loc['timestamp'][0][5]))
+        #print(stockDF.loc['timestamp'])
+
+        return stockDF
+
+    # you have to input the company code, such as AAPL,MSFT,TSLA
+    def get_Finance_Indicators_Data(self, company_symbols):
+
+        url = 'https://yfapi.net/v11/finance/quoteSummary/'+company_symbols
 
         query_string = {
-            'symbols': 'AAPL',
+            'symbols': company_symbols,
             'modules': 'financialData'
         }
 
@@ -46,13 +57,57 @@ class Control:
     
         stockData = modleObj.get_Finance_Data()
 
-        return self.parse_Data(stockData['quoteSummary']['result'][0]['financialData'])
+        stockDF = self.parse_Data(stockData['quoteSummary']['result'][0]['financialData'])
+
+        finance_Indicator = [{
+            'Name':'return On Equity',
+            'Value':stockDF['returnOnEquity']['fmt']
+        },{
+            'Name':'return On Assets',
+            'Value':stockDF['returnOnAssets']['fmt']
+        },{
+            'Name':'debt To Equity',
+            'Value':stockDF['debtToEquity']['fmt']
+        }]
+
+        return finance_Indicator
 
 
+    def get_Gernal_Market_Indexes(self):
+        url = 'https://yfapi.net/v6/finance/quote/marketSummary'
+
+        query_string = {
+            'lang': 'en',
+            'region': 'US'
+        }
+
+        modleObj = Model_Code.DataModel(url, query_string)
     
+        stockData = modleObj.get_Finance_Data()
+
+        stockDF = self.parse_Data(stockData['marketSummaryResponse']['result'])
+
+        Market_Index = [{
+            'Name':'S&P 500',
+            'Index': stockDF[stockDF['fullExchangeName']=='SNP']['regularMarketPrice'][0]['fmt']
+        },{
+            'Name':'Nasdaq Composite',
+            'Index': stockDF[stockDF['fullExchangeName']=='Nasdaq GIDS']['regularMarketPrice'][2]['fmt']
+        }]
+
+        return Market_Index
+
+        
 
 controlObj = Control()
-stockData = controlObj.get_ROE_Data()
+stockData = controlObj.get_Close_Price_Data()
 
-print(stockData['returnOnEquity'])
+print(stockData)
+#stockData = controlObj.get_Finance_Indicators_Data('TSLA')
+
+
+#print(stockData)
+
+# print(stockData['returnOnEquity'])
+
 # print(datetime.fromtimestamp(stockData['AAPL']['timestamp'][0]))
